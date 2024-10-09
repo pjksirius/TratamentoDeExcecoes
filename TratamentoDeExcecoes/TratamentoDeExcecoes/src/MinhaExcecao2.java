282d9d236c4460b809dba8d8a5fd78dedab3f12c
MAN DOWN <168221759+pjksirius@users.noreply.github.com>
2024-10-08 21:42:06 -0300

    Delete TratamentoDeExcecoes/TratamentoDeExcecoes directory

--- TratamentoDeExcecoes/TratamentoDeExcecoes/src/MinhaExcecao2.java
+++
@@ -1,9 +0,0 @@
-public class MinhaExcecao2 extends Exception{
-        public MinhaExcecao2(String mensagem) {
-            super(mensagem); 
-        }
-     
-        public void gerarExcecao() throws MinhaExcecao2 {
-            throw new MinhaExcecao2("Erro: Falha de Processamento em MinhaExcecao2");
-        }
-}
